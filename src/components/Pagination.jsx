ed554cee7c61a8c3ce0cd97a2c9abcfdb7e44c88
abhisek8094 <iasabhi41@gmail.com>
2024-06-21 01:04:12 +0530

    fix: page prev to nex

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { setPage } from '../redux/articleSlice';
 
 const Pagination = () => {
   const dispatch = useDispatch();
-  const page = useSelector((state)=> state.articles.page)
+  const page = useSelector((state)=> state.articles.page);
+
+  const handlePageChange = (newPage) => {
+    dispatch(setPage(newPage));
+  }
   return (
     <div>
-      <button onClick={() => handlePrevious(page-1)} disabled = {page ===1}>
+      <button onClick={() => handlePageChange(page-1)} disabled = {page ===1}>
       previous
       </button>
       <span>{page}</span>
-      <button>
+      <button onClick={() => handlePageChange(page + 1)}>
         Next
       </button>
     </div>
