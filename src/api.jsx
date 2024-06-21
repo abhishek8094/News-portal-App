963ad2b70608de0bcbaa356eb013ef01b0d064e6
abhisek8094 <iasabhi41@gmail.com>
2024-06-21 11:27:38 +0530

    api

--- src/api.jsx
+++ src/api.jsx
@@ -1,9 +1,11 @@
-const API_KEY = "37de4e32874b4dd6b4aa98518f117668";
+const API_KEY = "44b0a9c64c0f483e92dd8944f2bcd566";
 const BASE_URL = "https://newsapi.org/v2";
 
 const fetchArticles = async (category, page) => {
-    const url = `${BASE_URL}/top-headlines?country=us&category=${category}&page=${page}&apiKey=${API_KEY}`;
+    const url = `${BASE_URL}/top-headlines?country=In&category=${category}&page=${page}&apiKey=${API_KEY}`;
+    console.log(url);
     const response = await fetch(url);
+    console.log(response);
     if(!response.ok){
         throw new Error("failed to fetch articles");
     }
