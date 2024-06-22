55e2a6181d49ba4fb1b8a414a70e17c5703666d3
abhisek8094 <iasabhi41@gmail.com>
2024-06-22 10:21:14 +0530

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,43 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import fetchData from "../api";
+import fetchArticles from "../api";
 import { setArticles, setStatus, setError } from "../redux/articleSlice";
 import CategoryFilter from "../components/CategoryFilter";
 import ArticleCard from "../components/ArticleCard";
 import Pagination from "../components/Pagination";
+import { Circles } from 'react-loader-spinner';
 
 
 const Home = () => {
   const dispatch = useDispatch();
   const {articles, status, error, category, page} = useSelector((state) => state.articles)
 
-  
+  const loadArticles = async () => {
+    dispatch(setStatus("loading"));
+    try{
+      const data = await fetchArticles(category, page);
+      dispatch(setArticles(data.articles));
+      dispatch(setStatus("succeeded"));
+    }catch(err){
+      dispatch(setError(err.message));
+      dispatch(setStatus("failed"));
+    }
+  }
   
   useEffect(() => {
-    const loadArticles = async () => {
-      dispatch(setStatus("loading"));
-      try{
-        const data = await fetchData(category, page);
-        dispatch(setArticles(data.articles));
-        dispatch(setStatus("succeeded"));
-      }catch(err){
-        dispatch(setError(err.message));
-        dispatch(setStatus("failed"));
-      }
-    }
     loadArticles();
   },[category, page, dispatch]);
 
   if (status === 'loading') {
-    return <p>Loading...</p>;
+    return <p className="flex items-center justify-center h-screen mt-8"><Circles
+    height="80"
+    width="80"
+    color="#4fa94d"
+    ariaLabel="circles-loading"
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
+    /></p>;
   }
 
   if (status === 'failed') {
