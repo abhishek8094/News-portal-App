3ba115c96ddd5aebfb510f17b06566e539ec2d83
abhisek8094 <iasabhi41@gmail.com>
2024-06-22 10:22:55 +0530

    Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,17 +4,21 @@ import { setPage } from '../redux/articleSlice';
 const Pagination = () => {
   const dispatch = useDispatch();
   const page = useSelector((state)=> state.articles.page);
+  const articlesPerPage = useSelector((state) => state.articles.articlesPerPage);
+  const totalArticles = useSelector((state) => state.articles.articles.length);
 
   const handlePageChange = (newPage) => {
-    dispatch(setPage(newPage));
+    if (newPage >= 1 && newPage <= Math.ceil(totalArticles / articlesPerPage)) {
+      dispatch(setPage(newPage));
+    }
   }
   return (
     <div className="flex justify-center space-x-2 my-4">
-      <button className="px-4 py-2 rounded bg-gray-300" onClick={() => handlePageChange(page-1)} disabled = {page ===1}>
+      <button className="px-4 py-2 rounded bg-gray-300" onClick={() => handlePageChange(page-1)} >
       previous
       </button  >
       <span className="px-4 py-2">{page}</span>
-      <button className="px-4 py-2 rounded bg-gray-300" onClick={() => handlePageChange(page + 1)}>
+      <button className="px-4 py-2 rounded bg-gray-300" onClick={() => handlePageChange(page + 1)} disabled={page >= Math.ceil(totalArticles / articlesPerPage)}>
         Next
       </button>
     </div>
