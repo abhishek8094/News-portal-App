22f309e5545b55479fb95e8e9cc08b93e283cb46
abhisek8094 <iasabhi41@gmail.com>
2024-06-30 21:39:44 +0530

    api

--- src/api.jsx
+++ src/api.jsx
@@ -1,13 +1,15 @@
-const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
-const BASE_URL = "https://newsapi.org/v2";
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY; 
 
-const fetchArticles = async (category, page) => {
-  const url = `${BASE_URL}/top-headlines?country=in&category=${category}&page=${page}&apiKey=${API_KEY}`
-  // console.log(url)
-
+const fetchArticles = async () => {
   try {
-    const response = await fetch(url);
-    // console.log(response)
+    const response = await fetch('https://google-news13.p.rapidapi.com/business?lr=en-US', {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'google-news13.p.rapidapi.com',
+        'x-rapidapi-key': API_KEY
+      }
+    });
+    
     if (!response.ok) {
       throw new Error("Failed to fetch articles");
     }
